10be1c24dedb0634dd0126d22e4ff0c2651e3e09
gavendanoc <gabriel19oct@hotmail.com>
2020-12-13 10:13:27 -0500

    :bug: fixed sum

--- src/main/java/com/myerlang/erlapi/logic/MyVisitor.java
+++ src/main/java/com/myerlang/erlapi/logic/MyVisitor.java
@@ -176,6 +176,7 @@ public class MyVisitor<T> extends ErlangBaseVisitor {
             }
             Variable var = new Variable(variableName);
             this.scopes.peek().getScope().put(variableName, var);
+            var.getDatatype().setValue("something");
             return var.getDatatype();
         }
         if (isGettingVariableName) { // For pattern matching
